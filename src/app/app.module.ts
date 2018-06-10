ccc6b0c1b0e4ab5be9c098edc6171d21dc7cb14e
vgoreiko <mbox696@gmail.com>
2018-06-10 15:51:25 +0300

    added material module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import {AppComponent} from './app.component';
 import {RouterModule} from "@angular/router";
 import { AppRoutingModule } from './/app-routing.module';
 import {MainModule} from "./main/main.module";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
     RouterModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
     MainModule,
   ],
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SpinnerComponent } from './spinner/spinner.component';
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {MatInputModule} from "@angular/material";
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     ReactiveFormsModule,
+    MatInputModule,
   ],
   declarations: [SpinnerComponent],
   exports: [SpinnerComponent]
