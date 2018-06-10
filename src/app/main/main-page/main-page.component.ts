176f47cf5769109ec16ed74e4427ad6b98b8975c
vgoreiko <mbox696@gmail.com>
2018-06-10 14:37:15 +0300

    added spinner reactive component

--- src/app/main/main-page/main-page.component.ts
+++ src/app/main/main-page/main-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup} from "@angular/forms";
 
 @Component({
   selector: 'app-main-page',
@@ -7,7 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainPageComponent implements OnInit {
   count = 5
-  constructor() { }
+  counterGroup: FormGroup;
+  constructor(private fb: FormBuilder) {
+    this.counterGroup = this.fb.group({
+      count: [3]
+    })
+  }
 
   ngOnInit() {
   }
